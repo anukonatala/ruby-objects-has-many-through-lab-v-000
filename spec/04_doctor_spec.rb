0e0820632b41ac921b48491ea78ea6ec3db580ca
Anusha Konatala <anu.konatala@gmail.com>
2018-07-15 04:32:44 +0000

    automatically backed up by learn

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -43,7 +43,6 @@ describe "Doctor" do
       doctor_who = Doctor.new("The Doctor")
       hevydevy = Patient.new("Devin Townsend")
       doctor_who.new_appointment(hevydevy, "Friday, January 32nd")
-
       expect(doctor_who.patients).to include(hevydevy)
     end
   end
