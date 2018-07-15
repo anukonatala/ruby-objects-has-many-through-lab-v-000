b6cf1c9c22260b94dad4e558a83065e74932940f
Anusha Konatala <anu.konatala@gmail.com>
2018-07-15 04:31:27 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @patients = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient, time)
+    patient = Patient.new(patient) 
+    @patients << patient
+    appointment = Appointment.new(patient, self, time)
+  end
+  
+  
+  def appointments
+    Appointment.all.select {|a| a.doctor ==  self }
+  end
+  
+  def patients
+    @patients
+  end
+
+end
---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def new_appointment(doctor,date)
+    new_appointment = Appointment.new(self,doctor,date)
+    @appointments << new_appointment
+  end
+  
+  def appointments
+    @appointments
+  end
+  
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+end
